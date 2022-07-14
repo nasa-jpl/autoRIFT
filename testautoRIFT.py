d7b6aec4874d0dc8859c6c8922a80c0ea14bfcd6
Joseph H Kennedy <me@jhkennedy.org>
2022-07-14 10:57:07 -0800

    Prevent excessive nodata from entering into the search grid for Landsat-7

--- testautoRIFT.py
+++ testautoRIFT.py
@@ -188,14 +188,16 @@ def runAutorift(I1, I2, xGrid, yGrid, Dx0, Dy0, SRx0, SRy0, CSMINx0, CSMINy0, CS
         obj.xGrid = xGrid
         obj.yGrid = yGrid
 
-
-
+    # FIXME: This assumes the zero values in the image are only outside the valid image "frame",
+    #        but is not true for Landsat-7 after the failure of the Scan Line Corrector, May 31, 2003.
+    #        We should not masked based on zero values in the L7 images as this percolates into SearchLimit{X,Y}
+    #        and prevents autoRIFT from looking at large parts of the images.
     # generate the nodata mask where offset searching will be skipped based on 1) imported nodata mask and/or 2) zero values in the image
-    for ii in range(obj.xGrid.shape[0]):
-        for jj in range(obj.xGrid.shape[1]):
-            if (obj.yGrid[ii,jj] != nodata)&(obj.xGrid[ii,jj] != nodata):
-                if (I1[obj.yGrid[ii,jj]-1,obj.xGrid[ii,jj]-1]==0)|(I2[obj.yGrid[ii,jj]-1,obj.xGrid[ii,jj]-1]==0):
-                    noDataMask[ii,jj] = True
+    # for ii in range(obj.xGrid.shape[0]):
+    #     for jj in range(obj.xGrid.shape[1]):
+    #         if (obj.yGrid[ii,jj] != nodata)&(obj.xGrid[ii,jj] != nodata):
+    #             if (I1[obj.yGrid[ii,jj]-1,obj.xGrid[ii,jj]-1]==0)|(I2[obj.yGrid[ii,jj]-1,obj.xGrid[ii,jj]-1]==0):
+    #                 noDataMask[ii,jj] = True
 
 
 
