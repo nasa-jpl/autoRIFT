b0061da866646a435f24a193564d62881b94add7
Joseph H Kennedy <me@jhkennedy.org>
2025-05-16 13:51:07 -0800

    delete deprecated Geogrid.py

--- geo_autoRIFT/geogrid/__init__.py
+++ geo_autoRIFT/geogrid/__init__.py
@@ -1,14 +1,2 @@
-#!/usr/bin/env python
-
-#def SplitRangeSpectrum():
-#    from .splitSpectrum import PySplitRangeSpectrum
-#    return PySplitRangeSpectrum()
-
-# should always work - standalone or with ISCE
 from .GeogridOptical import GeogridOptical
 from .GeogridRadar import GeogridRadar
-try:
-    from .Geogrid import Geogrid
-except ImportError:
-    # this means ISCE support not available. Don't raise error. Allow standalone use
-    pass
